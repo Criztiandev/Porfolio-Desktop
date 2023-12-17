438d6d9ae38c12b99bab69b676007c7e02031d37
criztiandev <criztiandev@gmail.com>
2023-12-18 00:34:07 +0800

    FEAT: Initial Desktop Screen Setup

--- client/src/views/static/LandingScreen.tsx
+++ client/src/views/static/LandingScreen.tsx
@@ -1,5 +1,21 @@
 const LandingScreen = () => {
-  return <div>LandingScreen</div>;
+  return (
+    <div className="h-[100v] p-4">
+      <div className="absolute border-2 border-black w-[95%] m-auto bottom-5 px-4 py-2 flex justify-between items-center rounded-[5px]">
+        <div>
+          <button className="btn border-2 border-black">HI</button>
+        </div>
+
+        <div>
+          <button className="btn border-2 border-black">HI</button>
+        </div>
+
+        <div>
+          <button className="btn border-2 border-black">HI</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingScreen;
