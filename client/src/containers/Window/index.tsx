f4c921920a37b6fbc15b42a44cb127060f32e477
criztiandev <criztiandev@gmail.com>
2023-12-19 15:06:48 +0800

    Add: Close Render in the Component

--- client/src/containers/Window/index.tsx
+++ client/src/containers/Window/index.tsx
@@ -22,25 +22,34 @@ interface WindowComponent
 }
 const Window = forwardRef(
   ({ controls, children }: WindowProps, ref: ForwardedRef<HTMLDivElement>) => {
+    const [isClose, setIsClose] = useState(true);
     const [isMaximize, setIsMaximize] = useState(false);
     const containerRef = ref as Ref<HTMLDivElement>;
 
     return (
-      <motion.div
-        ref={containerRef}
-        drag
-        dragElastic={0}
-        dragMomentum={false}
-        dragControls={controls}
-        dragListener={false}
-        dragConstraints={ref ? (ref as RefObject<Element>) : false}
-        style={{
-          width: isMaximize ? "100%" : 800,
-          height: isMaximize ? "100%" : 500,
-        }}
-        className="absolute border-2 border-black rounded-md overflow-hidden bg-white">
-        {children}
-      </motion.div>
+      <>
+        {isClose && (
+          <motion.div
+            ref={containerRef}
+            drag
+            dragElastic={0}
+            dragMomentum={false}
+            dragControls={controls}
+            dragListener={false}
+            dragConstraints={ref ? (ref as RefObject<Element>) : false}
+            style={{
+              width: isMaximize ? "100%" : 800,
+              height: isMaximize ? "100%" : 500,
+            }}
+            className="absolute border-2 border-black rounded-md overflow-hidden bg-white">
+            {children}
+            <button onClick={() => setIsClose((prev) => !prev)}>Close</button>
+            <button onClick={() => setIsMaximize((prev) => !prev)}>
+              Maximize
+            </button>
+          </motion.div>
+        )}
+      </>
     );
   }
 ) as WindowComponent;
